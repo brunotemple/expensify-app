ffa7150c2c71adb5387020e54f5b16d724571a19
unknown <bruno.temple@gmail.com>
2017-11-30 23:49:39 +0800

    Setup prodiction webpack build

--- src/app.js
+++ src/app.js
@@ -12,23 +12,10 @@ import 'react-dates/lib/css/_datepicker.css';
 
 const store = configureStore();
 
-//store.dispatch(addExpense({ description: 'Water Bill', amount: 4500, createdAt: 21000 }));
-//store.dispatch(addExpense({ description: 'Gas Bill', amount: 3000, createdAt: 1500 }));
-//store.dispatch(addExpense({ description: 'Rent', amount: 109500, createdAt: 1000 }));
-
-// store.dispatch(setTextFilter('water'));
-// setTimeout(() => {
-//   store.dispatch(setTextFilter('bill'));
-// },3000)
-
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-console.log(visibleExpenses);
-
 const jsx = (
   <Provider store={store}>
     <AppRouter />
   </Provider>
 );
 
-ReactDOM.render(jsx , document.getElementById('app'));
+ReactDOM.render(jsx, document.getElementById('app'));
